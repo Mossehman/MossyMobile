a34f474fe3b4669a81c618343806ad80edb50ad7
Mossehman <134377085+Mossehman@users.noreply.github.com>
2025-01-03 23:51:16 +0800

    a

--- app/src/main/java/com/example/mossymobile/MossFramework/Systems/Files/SerializerTest.java
+++ app/src/main/java/com/example/mossymobile/MossFramework/Systems/Files/SerializerTest.java
@@ -6,6 +6,11 @@ public class SerializerTest {
     public static boolean IsExternalStorageReadable()
     {
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) ||
-        Environment)
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(Environment.getExternalStorageState()))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
